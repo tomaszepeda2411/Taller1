73e2a65c34b3ba8ceb68c7482fd9c45a7e4a0631
tomaszepeda2411 <tomas.zepeda01@alumnos.ucn.cl>
2026-09-10 19:31:04 -0300

    Menu interactivo y carga de archivos

--- src/taller/main.java
+++ src/taller/main.java
@@ -7,10 +7,39 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class main {
-
+	
 	public static void main(String[] args) {
-		leerSolicitudes();
-		leerAlumnos();
+		int opcion = 0;
+		Scanner scan = new Scanner(System.in);
+		do {
+		System.out.println("==== Sistema de Control del Grupo POO ====");
+		System.out.println("1) Cargar archivos  (Alumnos y solicitudes)");
+		System.out.println("2) Procesar solicitudes (Filtrado automatico");
+		System.out.println("3) Inscripcion manual al grupo");
+		System.out.println("4) Administracion del curso");
+		System.out.println("5) Generar reportes");
+		System.out.println("6) Analisis estadistico");
+		System.out.println("7) Salir");
+		opcion = scan.nextInt();
+		switch(opcion) {
+		case 1:
+			leerSolicitudes();
+			leerAlumnos();
+		case 2:
+		
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+			
+		case 7:
+			break;
+		default:
+			System.out.println("Ingrese una opcion Valida!!!");
+			
+		}
+		}while(opcion!=7);
+		
 		
 	}
 
@@ -25,10 +54,11 @@ public class main {
 			String apellidoA = partes[1];
 			String rut = partes[2];
 			String paralelo = partes[3];
-		}scan.close();
+			
+		}System.out.println("Archivo Alumnos cargado correctamente!");
+		scan.close();
 	} catch (FileNotFoundException e) {
-		System.out.println("Archivo no encontrado");
-		e.printStackTrace();
+		System.out.println("Archivo Alumnos no encontrado");
 	}
 	}
 
@@ -41,10 +71,10 @@ public class main {
 				String[] partes = linea.split("-");
 				String nombreS = partes[0];
 				String apellidoS = partes[1];
-			}scan.close();
+			}System.out.println("Archivo Solicitudes cargado correctamente!");
+			scan.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("No se ha podido encontrar el archivo");
-			e.printStackTrace();
 		}
 		
 	}
